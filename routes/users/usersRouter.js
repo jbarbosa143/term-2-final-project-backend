18ecaca2fec160584da4c0b86ca63bebf9ead0d0
jeremybarbosa <jbarbosa1430@gmail.com>
2021-08-23 10:01:09 -0500

    backend

---
+++ routes/users/usersRouter.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var { createUser, login } = require("./controller/userController");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/create-user", createUser);
+router.post("/login", login);
+router.put(
+  "/update-profile",
+  passport.authenticate("jwt-user", { session: false }),
+  function (req, res) {
+    res.send("YAYYYYY!@@!#!@#!@#!@#!@#!@#!@#!@#!@#");
+  }
+);
+router.get("/logout", function (req, res) {
+  res.clearCookie("jwt-cookie");
+  res.send("Logged Out!");
+});
+
+module.exports = router;
